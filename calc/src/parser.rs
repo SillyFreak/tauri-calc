79b3aabe44d23b72c3109e43eea40050e1a356d3
Clemens Koza <clemens.koza@gmx.at>
2021-12-31 22:53:19 +0100

    values can now be empty, string or number

--- calc/src/parser.rs
+++ calc/src/parser.rs
@@ -5,38 +5,36 @@ mod range;
 mod string;
 
 use nom::branch::alt;
-use nom::combinator::map;
+use nom::combinator::{eof, map};
 use nom::IResult;
 
 use crate::value::Value;
 
-pub fn parse(input: &str) -> IResult<&str, Option<Value>> {
-    let formula = map(formula::parse_formula, |f| Some(Value::Formula(f)));
-    let number = map(number::parse_number, |n| Some(Value::Number(n)));
-    let string = map(string::parse_string, |s| Some(Value::String(s)));
+pub fn parse_value(input: &str) -> IResult<&str, Value> {
+    let empty = map(eof, |_| Value::Empty);
+    let number = map(number::parse_number, |n| Value::Number(n));
+    let string = map(string::parse_string, |s| Value::String(s));
 
-    alt((formula, number, string))(input)
+    alt((empty, number, string))(input)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    use crate::address::CellAddress;
-    use crate::value::{Formula, Value};
+    use crate::value::Value;
 
     #[test]
     fn test_parse() {
+        assert_eq!(parse_value("").unwrap().1, Value::Empty);
         assert_eq!(
-            parse("1").unwrap().1.unwrap(),
+            parse_value("1").unwrap().1,
             Value::Number("1".parse().unwrap())
         );
         assert_eq!(
-            parse("\"foo\"").unwrap().1.unwrap(),
+            parse_value("\"foo\"").unwrap().1,
             Value::String("foo".to_string())
         );
-        let cell_address = CellAddress::new(1.try_into().unwrap(), 1.try_into().unwrap());
-        let formula = Value::Formula(Formula::Reference(cell_address));
-        assert_eq!(parse("=A1").unwrap().1.unwrap(), formula);
+        assert!(matches!(parse_value("x"), Err(_)));
     }
 }
--- calc/src/value.rs
+++ calc/src/value.rs
@@ -8,17 +8,17 @@ pub use formula::Formula;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Value {
+    Empty,
     Number(BigDecimal),
     String(String),
-    Formula(Formula),
 }
 
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Self::Empty => write!(f, ""),
             Self::Number(value) => write!(f, "{}", value),
             Self::String(value) => write!(f, "{:?}", value),
-            Self::Formula(value) => write!(f, "={}", value),
         }
     }
 }
@@ -27,14 +27,10 @@ impl fmt::Display for Value {
 mod test {
     use super::*;
 
-    use crate::address::CellAddress;
-
     #[test]
     fn test_value() {
+        assert_eq!(format!("{}", Value::Empty), "");
         assert_eq!(format!("{}", Value::Number("1".parse().unwrap())), "1");
         assert_eq!(format!("{}", Value::String("foo".to_string())), "\"foo\"");
-        let cell_address = CellAddress::new(1.try_into().unwrap(), 1.try_into().unwrap());
-        let formula = Value::Formula(Formula::Reference(cell_address));
-        assert_eq!(format!("{}", formula), "=A1");
     }
 }
