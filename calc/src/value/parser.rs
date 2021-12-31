7d7ba2ed505ad4b1c3f07f4ed223369dbbb6c28c
Clemens Koza <clemens.koza@gmx.at>
2021-12-31 13:33:53 +0100

    PICK ME: parser

--- calc/src/value/parser.rs
+++ calc/src/value/parser.rs
@@ -20,7 +20,10 @@ mod test {
 
     #[test]
     fn test_parse() {
-        assert_eq!(parse("1").unwrap().1.unwrap(), Value::Number("1".parse().unwrap()));
+        assert_eq!(
+            parse("1").unwrap().1.unwrap(),
+            Value::Number("1".parse().unwrap())
+        );
         assert_eq!(
             parse("\"foo\"").unwrap().1.unwrap(),
             Value::String("foo".to_string())
