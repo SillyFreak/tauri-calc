e480573cab18683a1fd7c68de5d6517b37340446
Clemens Koza <clemens.koza@gmx.at>
2021-12-31 11:17:00 +0100

    format code, add some derives

--- calc/src/address.rs
+++ calc/src/address.rs
@@ -1,115 +1,115 @@
 use std::fmt;
 use std::num::NonZeroU32;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct RowAddress(NonZeroU32);
 
 impl RowAddress {
-  pub fn new(address: NonZeroU32) -> Self {
-    Self(address)
-  }
+    pub fn new(address: NonZeroU32) -> Self {
+        Self(address)
+    }
 }
 
 impl From<NonZeroU32> for RowAddress {
-  fn from(address: NonZeroU32) -> Self {
-    Self::new(address)
-  }
+    fn from(address: NonZeroU32) -> Self {
+        Self::new(address)
+    }
 }
 
 impl TryFrom<u32> for RowAddress {
-	type Error = ();
+    type Error = ();
 
-  fn try_from(address: u32) -> Result<Self, Self::Error> {
-		let address = NonZeroU32::new(address).ok_or(())?;
-		Ok(Self::new(address))
-  }
+    fn try_from(address: u32) -> Result<Self, Self::Error> {
+        let address = NonZeroU32::new(address).ok_or(())?;
+        Ok(Self::new(address))
+    }
 }
 
 impl fmt::Display for RowAddress {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "{}", self.0)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ColAddress(NonZeroU32);
 
 impl ColAddress {
-  pub fn new(address: NonZeroU32) -> Self {
-    Self(address)
-  }
+    pub fn new(address: NonZeroU32) -> Self {
+        Self(address)
+    }
 }
 
 impl TryFrom<u32> for ColAddress {
-	type Error = ();
+    type Error = ();
 
-  fn try_from(address: u32) -> Result<Self, Self::Error> {
-		let address = NonZeroU32::new(address).ok_or(())?;
-		Ok(Self::new(address))
-  }
+    fn try_from(address: u32) -> Result<Self, Self::Error> {
+        let address = NonZeroU32::new(address).ok_or(())?;
+        Ok(Self::new(address))
+    }
 }
 
 impl fmt::Display for ColAddress {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		// convert the address into a 0-based number so that we can treat 'A' as the digit 0
-		let mut num = u32::from(self.0) - 1;
-		// we need at least one byte, so allocate up front
-		// more than four (always ASCII) characters are only needed for outlandish numbers of columns
-		let mut str = String::with_capacity(4);
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // convert the address into a 0-based number so that we can treat 'A' as the digit 0
+        let mut num = u32::from(self.0) - 1;
+        // we need at least one byte, so allocate up front
+        // more than four (always ASCII) characters are only needed for outlandish numbers of columns
+        let mut str = String::with_capacity(4);
 
-		fn base26digit(num: u32) -> Option<char> {
-			// converting a number in 10..36 with radix 36 will result in a base 26 letter
-			let digit = char::from_digit(num + 10, 36)?;
-			let digit = digit.to_ascii_uppercase();
-			Some(digit)
-		}
+        fn base26digit(num: u32) -> Option<char> {
+            // converting a number in 10..36 with radix 36 will result in a base 26 letter
+            let digit = char::from_digit(num + 10, 36)?;
+            let digit = digit.to_ascii_uppercase();
+            Some(digit)
+        }
 
-		if num == 0 {
-			let digit = base26digit(0).unwrap();
-			str.push(digit);
-		} else {
-			while num > 0 {
-				let digit = base26digit(num % 26).unwrap();
-				str.insert(0, digit);
-				num /= 26;
-			}
-		}
+        if num == 0 {
+            let digit = base26digit(0).unwrap();
+            str.push(digit);
+        } else {
+            while num > 0 {
+                let digit = base26digit(num % 26).unwrap();
+                str.insert(0, digit);
+                num /= 26;
+            }
+        }
 
-		write!(f, "{}", str)
-	}
+        write!(f, "{}", str)
+    }
 }
 
 impl From<NonZeroU32> for ColAddress {
-  fn from(address: NonZeroU32) -> Self {
-    Self::new(address)
-  }
+    fn from(address: NonZeroU32) -> Self {
+        Self::new(address)
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct CellAddress {
-  row: RowAddress,
-  col: ColAddress,
+    row: RowAddress,
+    col: ColAddress,
 }
 
 impl CellAddress {
-  pub fn new(row: RowAddress, col: ColAddress) -> Self {
-    Self { row, col }
-  }
+    pub fn new(row: RowAddress, col: ColAddress) -> Self {
+        Self { row, col }
+    }
 }
 
 impl fmt::Display for CellAddress {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "{}{}", self.col, self.row)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.col, self.row)
+    }
 }
 
 #[cfg(test)]
 mod test {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn test_display() {
-		let cell_address = CellAddress::new(1.try_into().unwrap(), 1.try_into().unwrap());
-		assert_eq!(format!("{}", cell_address), "A1");
-	}
+    #[test]
+    fn test_display() {
+        let cell_address = CellAddress::new(1.try_into().unwrap(), 1.try_into().unwrap());
+        assert_eq!(format!("{}", cell_address), "A1");
+    }
 }
--- calc/src/value.rs
+++ calc/src/value.rs
@@ -2,48 +2,49 @@ use std::fmt;
 
 use crate::address::CellAddress;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Formula {
-	Reference(CellAddress),
+    Reference(CellAddress),
 }
 
 impl fmt::Display for Formula {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match self {
-			Self::Reference(value) => write!(f, "{}", value),
-		}
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Reference(value) => write!(f, "{}", value),
+        }
+    }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Value {
-	Integer(i64),
-	Decimal(f64),
-	String(String),
-	Formula(Formula),
+    Integer(i64),
+    Decimal(f64),
+    String(String),
+    Formula(Formula),
 }
 
 impl fmt::Display for Value {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match self {
-			Self::Integer(value) => write!(f, "{:?}", value),
-			Self::Decimal(value) => write!(f, "{:?}", value),
-			Self::String(value) => write!(f, "{:?}", value),
-			Self::Formula(value) => write!(f, "={}", value),
-		}
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Integer(value) => write!(f, "{:?}", value),
+            Self::Decimal(value) => write!(f, "{:?}", value),
+            Self::String(value) => write!(f, "{:?}", value),
+            Self::Formula(value) => write!(f, "={}", value),
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn test_value() {
-		assert_eq!(format!("{}", Value::Integer(1)), "1");
-		assert_eq!(format!("{}", Value::Decimal(1.0)), "1.0");
-		assert_eq!(format!("{}", Value::String("foo".to_string())), "\"foo\"");
-		let cell_address = CellAddress::new(1.try_into().unwrap(), 1.try_into().unwrap());
-		assert_eq!(format!("{}", Value::Formula(Formula::Reference(cell_address))), "=A1");
-	}
+    #[test]
+    fn test_value() {
+        assert_eq!(format!("{}", Value::Integer(1)), "1");
+        assert_eq!(format!("{}", Value::Decimal(1.0)), "1.0");
+        assert_eq!(format!("{}", Value::String("foo".to_string())), "\"foo\"");
+        let cell_address = CellAddress::new(1.try_into().unwrap(), 1.try_into().unwrap());
+        let formula = Value::Formula(Formula::Reference(cell_address));
+        assert_eq!(format!("{}", formula), "=A1");
+    }
 }
