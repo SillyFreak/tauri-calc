875b73ff0e7c9f835756d7b43f6627eaf7caa740
Clemens Koza <clemens.koza@gmx.at>
2021-12-31 10:47:00 +0100

    add calc library crate

--- calc-app/src-tauri/src/main.rs
+++ calc-app/src-tauri/src/main.rs
@@ -3,9 +3,6 @@
   windows_subsystem = "windows"
 )]
 
-mod address;
-mod value;
-
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
---
+++ calc/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod address;
+pub mod value;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
